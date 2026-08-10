9b40ac47b6aced06d34ab63179339929b3adda5d
manirathnam5 <manirathnam501@gmail.com>
2026-08-10 21:26:49 +0530

    js file

--- app.js
+++ app.js
@@ -1,5 +1,69 @@
- alert("Learn Web Programming");
+
  console.log("crypto");
  console.log("binance");
 
+ let username;
  
+ username="Manish";
+
+ console.log(username);
+
+
+ const comp = " Limitless Dev Lab"
+
+ console.log(comp);
+
+ let flatname="Geetana Iris ";
+
+ console.log(flatname);
+
+ let name="youtuber";
+
+ console.log(name);
+
+ let age = 23;
+
+ console.log(" Age "+age);
+
+
+ console.log(" Age condition "+ age > 18 );
+
+ if(age > 18)
+    console.log("you are adult ");
+
+
+
+ let counter = 1;
+
+  while(counter < 10){
+     console.log(counter);
+     counter++;
+  }
+
+
+
+  function sayHello(name){
+
+    console.log("Hello " + name );
+
+  }
+
+
+  sayHello("Manish");
+  sayHello("SARA");
+  sayHello("Tara");
+  sayHello("Nike");
+
+
+  function add(num1 , num2){
+    console.log(num1+num2);
+  }
+
+  add(4,5);
+
+  add(44,55);
+
+  add(7,6);
+
+  console.log(document);
+  console.dir(document);
